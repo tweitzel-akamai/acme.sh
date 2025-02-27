2242a1748e77ecac3488dd2efbca632f4bc33f5e
Weitzel, Tod <tweitzel@akamai.com>
2025-02-27 14:54:30 -0800

    Pass through text unchanged if no way to convert it to utf-8 exists.

--- dnsapi/dns_edgedns.sh
+++ dnsapi/dns_edgedns.sh
@@ -440,8 +440,8 @@ _edgedns_iconv_t_utf_8() {
       _debug2 "uconv missing"
       if ! _exists perl; then
         _debug2 "perl missing"
-        _err "iconv or uconv or perl not found"
-        return 1
+        _err "iconv or uconv or perl not found, hopefully this is utf8"
+        __returnval="$@"
       fi
     fi
   fi
@@ -465,7 +465,7 @@ _edgedns_iconv_f_utf_8() {
       if ! _exists perl; then
         _debug2 "perl missing"
         _err "iconv or uconv or perl not found"
-        return 1
+        __returnval="$@"
       fi
     fi
   fi
